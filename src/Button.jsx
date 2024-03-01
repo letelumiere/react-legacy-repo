83af1dc5935f80b14ead4e4ad927a9ffacce4d42
letelumiere <letelumiere@gmail.com>
2024-03-01 20:45:15 +0900

    init commits.

---
+++ src/Button.jsx
@@ -0,0 +1,26 @@
+
+
+function Button(){
+    const handleClick = (e) => e.target.textContent = "Ouch!";
+
+    return(<button onDoubleClick={(e)=> handleClick(e)}>Click me!</button>);
+
+}
+
+/*
+function Button(){
+
+    let count = 0;
+    const handleClick = (name) => {
+        if(count<3){
+            count++;
+            console.log(`${name} you clicked me ${count} time/s`);
+        }else{
+            console.log(`${name} stop clicking me!`);
+        }
+    };
+
+    return(<button onClick={() => handleClick("Bro")}>Click me </button>);
+}
+*/
+export default Button;
